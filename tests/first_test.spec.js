ccd96dda4a08d1f811757ef83baad890d3465464
Jesus Garcia Rojas <jegaro10@MacBook-Pro-de-Jesus.local>
2025-08-28 11:46:44 -0600

    Initial commit of clean project

---
+++ tests/first_test.spec.js
@@ -0,0 +1,18 @@
+// Importamos las funciones 'test' y 'expect' de Playwright.
+// 'test' define un bloque de prueba y 'expect' se usa para las aserciones.
+const { test, expect } = require('@playwright/test');
+
+// Definimos un bloque de prueba. El primer argumento es una descripción del test.
+test('has title', async ({ page }) => {
+
+  // Navega a la URL de Google.
+  await page.goto('https://www.google.com');
+
+  // Espera a que la página se cargue completamente y verifica que el título sea 'Google'.
+  // 'expect' es una aserción que detendrá el test si la condición no se cumple.
+  await expect(page).toHaveTitle(/Google/);
+
+  // Opcional: Tomamos una captura de pantalla y la guardamos en la carpeta 'test-results/screenshots'.
+  // Esto es útil para verificar visualmente que la prueba se ejecutó.
+  await page.screenshot({ path: 'test-results/screenshot.png' });
+});
