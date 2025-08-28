4c90aa85beb359b0ff1ed9a66f912520e03213af
Jesus Garcia Rojas <jegaro10@MacBook-Pro-de-Jesus.local>
2025-08-28 16:46:21 -0600

    fix: placeholder de formulario

--- tests/form_test.spec.js
+++ tests/form_test.spec.js
@@ -1,5 +1,5 @@
 // Importamos los módulos 'test' y 'expect' de Playwright.
-const { test, expect } = Require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 // Definimos la prueba con un nombre claro que describe su propósito.
 test('has a functional contact form', async ({ page }) => {
